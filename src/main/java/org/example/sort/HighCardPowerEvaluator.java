5bf548645038d228205ca53dc2c81ba889c6069f
thuggeelya <ilya-fdmdh@mail.ru>
2022-06-07 15:47:48 +0700

    review

--- src/main/java/org/example/sort/HighCardPowerEvaluator.java
+++ src/main/java/org/example/sort/HighCardPowerEvaluator.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * Class is used five-card combination power evaluation.
+ * Class is used for five-card combination power evaluation.
  */
 public class HighCardPowerEvaluator extends PokerHandPowerEvaluator {
 
