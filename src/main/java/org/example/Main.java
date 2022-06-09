0b23f22761581ba3c085a84eda81d8add90a3794
thuggeelya <ilya-fdmdh@mail.ru>
2022-06-09 18:57:42 +0700

    review

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        PokerHand hand = new PokerHand(args[0]);
-        System.out.println(hand + " " + hand.getCombination());
+        PokerHand hand1 = new PokerHand(args[0]);
+        PokerHand hand2 = new PokerHand(args[1]);
+        System.out.println(hand1 + " " + hand1.getCombination());
+        System.out.println(hand2 + " " + hand2.getCombination());
     }
 }
